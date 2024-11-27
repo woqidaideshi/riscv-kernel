b14e0b1be46f0d3a669f10138b895b824158e32e
Yafen Fang <yafen@iscas.ac.cn>
2024-11-27 17:32:12 +0800

    github ci init

---
+++ .buildkite/scripts/kernel-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make O=./build ARCH=riscv openeuler_defconfig
+make O=./build ARCH=riscv -j$(nproc) Image modules dtbs
+make O=./build INSTALL_MOD_PATH=./install modules_install
+mkdir -p ./install/boot/dtb/thead
+cp ./build/arch/riscv/boot/Image ./install/boot/
+find ./build/arch/riscv/boot/dts/ -name *.dtb | xargs -i cp {} ./install/boot/dtb/
+cp ./install/boot/dtb/thead*.dtb ./install/boot/dtb/thead
+tar -cvf riscv-$(date +"%Y%m%d%H%M").tar -C ./install .
